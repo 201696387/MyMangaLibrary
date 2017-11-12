71c859d15dfbc270e733dd9f3aee5c67da57df58
JFMaturana <jfmaturana@gmail.com>
2017-11-12 07:04:13 -0500

    07h03 @ 12/11/2017
    Fixed missing FinalVariables class

---
+++ app/src/main/java/ca/jfmcode/mymangalibrary/System/FinalVariables.java
@@ -0,0 +1,31 @@
+package ca.jfmcode.mymangalibrary.System;
+
+/**
+ * Created by ONi on 11/11/2017.
+ */
+
+public class FinalVariables { //NOTE: not ignored from Android Studio integrated gitignore to show how to gather all final variables in one convenient place
+
+    //region LaunchActivity ForResult codes
+    public static final int LOGINREQCODE = 123;
+    public static final int EXITAPPCODE = 1;
+    public static final int UPDATEMANGAINFOCODE = 7;
+    //endregion
+
+    //region MangaList ForResult codes
+    public static final int MANGACODE = 456;
+    public static final int MANGASAVED = 457;
+    //endregion
+
+    public static final String SIGNUPURL = "https://myanimelist.net/register.php?from=%2F";
+
+    public static final String MALURL = "myanimelist.net";
+
+    //region System files hashmap keys
+    public static final String IDKEY = "ident456";
+    public static final String USERNAMEKEY = "usr789";
+    public static final String PASSWORDKEY = "pss159";
+    //endregion
+
+    public static final String MANGALISTKEY = "mngalist153";
+}
